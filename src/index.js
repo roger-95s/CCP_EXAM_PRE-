1445f3949133510694a84158c413540ab1aac495
Roger E. Campo Cordova <rogeresleitercc@gmail.com>
2025-12-12 07:35:36 -0800

    Delete src directory
    No longer need

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import ClfPracticeApp from './aws_exam_app_with_timer';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ClfPracticeApp />);
